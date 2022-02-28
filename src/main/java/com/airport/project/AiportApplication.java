a7f8470b63d51f21333e8a4b7a49fdc6a3b91a6b
jonassantos1000 <jonasvale3@hotmail.com>
2022-02-28 15:26:12 -0300

    Dynamic path datasource implemented

--- src/main/java/com/airport/project/AiportApplication.java
+++ src/main/java/com/airport/project/AiportApplication.java
@@ -1,7 +1,14 @@
 package com.airport.project;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class AiportApplication {
@@ -10,4 +17,57 @@ public class AiportApplication {
 		SpringApplication.run(AiportApplication.class, args);
 	}
 
+    private static String getBase() {
+    	try {
+	        String path = ".\\datasource.ini";
+	        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+	            String line = null;
+	            line = br.readLine();
+	            return line;
+	        }
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    }
+    
+    private static String getUser() {
+    	try {
+	        String path = ".\\datasource.ini";
+	        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+	            String line = null;
+	            line = br.readLine();
+	            line = br.readLine();
+	            return line;
+	        }
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    }
+    
+    private static String getPassword() {
+    	try {
+	        String path = ".\\datasource.ini";
+	        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+	            String line = null;
+	            line = br.readLine();
+	            line = br.readLine();
+	            line = br.readLine();
+	            return line;
+	        }
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    }
+        
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://"+getBase());
+        dataSource.setUsername(getUser());
+        dataSource.setPassword(getPassword());
+        return dataSource;
+    }
 }
--- src/test/java/com/aiport/project/AiportApplicationTests.java
+++ src/test/java/com/aiport/project/AiportApplicationTests.java
@@ -3,7 +3,9 @@ package com.aiport.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.airport.project.AiportApplication;
+
+@SpringBootTest(classes = AiportApplication.class)
 class AiportApplicationTests {
 
 	@Test
